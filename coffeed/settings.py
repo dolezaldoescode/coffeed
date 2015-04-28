f9f186827ec684a5f4b7228d2de6fda88f78ee60
Patrick Dolezal <p3dolezal@gmail.com>
2015-04-28 19:04:07 -0400

    Update

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -112,7 +112,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(MAIN_DIR, 'static'),
-)
+
+STATICFILES_DIRS = (
+    os.path.join(MAIN_DIR, 'static'),
+    )
 
 STATIC_ROOT = 'staticfiles'
